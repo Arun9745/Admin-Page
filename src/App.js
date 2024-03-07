b04aabbe2be9997f2bdae6b13a69556cd09e22bf
Arun Prasanth E <arunprasanthmoorthi@gmail.com>
2024-03-07 17:01:48 +0530

    dd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Adding from './Adding Questions/Adding';
+// import Side from './Sidebar/Side';
+import Home from './Sidebar/Pages/Home';
+import Reports from './Sidebar/Pages/Reports';
+import Questions from './Sidebar/Pages/Questions';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+import Navbar from './Sidebar/Components/Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+   <div>
+   <Router>
+      <Navbar />
+      <Adding />
+      <Routes>
+      <Route path='/' exact component={<Home />} />
+          <Route path='/survey' component={<Reports />} />
+          <Route path='/questions' component={<Questions />} />
+          </Routes>
+   </Router>
+   </div>
   );
-}
+  
+};
 
 export default App;
---
+++ src/Sidebar/Side.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Side.css';
+import Navbar from './Components/Navbar';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Reports from './Pages/Reports';
+import Products from './Pages/Questions';
+
+function Side() {
+  return (
+    <>
+      {/* <Router>
+        <Navbar />
+          <Routes>
+          <Route path='/' exact component={<Home />} />
+          <Route path='/reports' component={<Reports />} />
+          <Route path='/products' component={<Products />} />
+          </Routes>
+      </Router> */}
+      <h1>Hiiiiiii</h1>
+    </>
+  );
+}
+
+export default Side;
